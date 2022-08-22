937779a475e8a2463c6166c01d4b356fe179df55
utkarshsaxena28 <utkarshsaxena28@gmail.com>
2022-08-22 12:07:44 +0530

    second commit

--- src/main/java/com/calendar/CalendarApplication.java
+++ src/main/java/com/calendar/CalendarApplication.java
@@ -1,8 +1,8 @@
 package com.calendar;
 
-import org.junit.Test;
+//import org.junit.Test;
 
-import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
